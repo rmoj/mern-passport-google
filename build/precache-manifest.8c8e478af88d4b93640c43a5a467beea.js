c9171ccdd2180fcb741ad68b400c6f9f7b692270
rmoj <rmoj99@gmail.com>
2019-05-25 05:06:06 -0400

    Initial commit

---
+++ build/precache-manifest.8c8e478af88d4b93640c43a5a467beea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b663fb3bd950edb9d56649fd299c2ae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f5f77f2b5c4567ad33a",
+    "url": "/static/css/main.4575ae16.chunk.css"
+  },
+  {
+    "revision": "d3ed2f7817ba7ac1c145",
+    "url": "/static/js/2.74a9bd10.chunk.js"
+  },
+  {
+    "revision": "4f5f77f2b5c4567ad33a",
+    "url": "/static/js/main.7b695d84.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
